5f5e96594ee954464ce2d25cecad6fb777d5365d
Svitlana D <svetk2801@gmail.com>
2023-01-08 18:11:55 +0200

    module 4.1 task 3

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        if (symbol == 'A' || symbol == 'B' || symbol == 'C' || symbol == 'D' || symbol == 'E' || symbol == 'F' || symbol == 'G'
+                || symbol == 'H' || symbol == 'I' || symbol == 'J' || symbol == 'K' || symbol == 'L' || symbol == 'M'
+                || symbol == 'N' || symbol == 'O' || symbol == 'P' || symbol == 'Q' || symbol == 'R' || symbol == 'S'
+                || symbol == 'T' || symbol == 'U' || symbol == 'V' || symbol == 'W' || symbol == 'X' || symbol == 'Y'
+                || symbol == 'Z'
+                || symbol == 'a' || symbol == 'b' || symbol == 'c' || symbol == 'd' || symbol == 'e' || symbol == 'f' || symbol == 'g'
+                || symbol == 'h' || symbol == 'i' || symbol == 'j' || symbol == 'k' || symbol == 'l' || symbol == 'm'
+                || symbol == 'n' || symbol == 'o' || symbol == 'p' || symbol == 'q' || symbol == 'r' || symbol == 's'
+                || symbol == 't' || symbol == 'u' || symbol == 'v' || symbol == 'w' || symbol == 'x' || symbol == 'y'
+                || symbol == 'z') {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double salaryAfterTaxes = 0;
+        if(salary > 0 && salary <= 10000){
+            salaryAfterTaxes = salary - (salary / 100 * 15);
+            System.out.println(salaryAfterTaxes);
+        } else if (salary > 10000 && salary <= 20000){
+            salaryAfterTaxes = salary - (salary / 100 * 18);
+            System.out.println(salaryAfterTaxes);
+        } else if (salary > 20000) {
+            salaryAfterTaxes = salary - (salary / 100 * 20);
+            System.out.println(salaryAfterTaxes);
+        } else {
+            System.out.println("wrong input!");
+        }
     }
 }
